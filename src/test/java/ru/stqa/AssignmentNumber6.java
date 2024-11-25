44bbb176d0c1c06116e676c3e032474e94161375
Oleg <webogee@gmail.com>
2024-11-25 00:13:13 -0500

    first test push

--- src/test/java/ru/stqa/AssignmentNumber6.java
+++ src/test/java/ru/stqa/AssignmentNumber6.java
@@ -35,16 +35,6 @@ public class AssignmentNumber6 {
 
     }
 
-    public void clickElementWithWait(WebDriver driver, By by) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3)); // 10 seconds timeout
-        try {
-            WebElement element = wait.until(ExpectedConditions.elementToBeClickable(by));
-            element.click();
-        } catch (Exception e) {
-            System.out.println("Element not clickable: " + e.getMessage());
-        }
-    }
-
 
     @Test
     public void MyVeryFirstTest() throws InterruptedException {
@@ -56,41 +46,34 @@ public class AssignmentNumber6 {
             wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.logotype")));
             System.out.println("Page verified");
             final int maxRetries = 3;
-
-            //List<WebElement> mainMenuItems = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("li#app-"))); // Adjust selector for main menu items
-
-            // Iterate over each main menu item
             List<WebElement> mainMenuItems = driver.findElements(By.cssSelector("li#app-"));
-            System.out.println("Mane Menu contains " + (mainMenuItems.size()) + " items.");
+            System.out.println("Main Menu contains " + (mainMenuItems.size()) + " items.");
             System.out.println("------------------------------------------------");
 
             for (int i = 0; i < mainMenuItems.size(); i++) {
 
                 mainMenuItems = driver.findElements(By.cssSelector("li#app-"));
+                String title = mainMenuItems.get(i).getText();
                 mainMenuItems.get(i).click();
-                //WebElement spanName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='name']")));
                 WebElement h1 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h1")));
                 System.out.println("------------------------------------------------");
-                System.out.println("Clicked on the Main Menu Item # " + (i + 1));
+                System.out.println("Clicked on the Main Menu Item # " + (i+1) + " << " + title + " >>" );
                 System.out.println("Page Title is " + h1.getText());
-
                 List<WebElement> subMenuItems = driver.findElements(By.cssSelector("li[id^=doc-"));
                 System.out.println("This Main Menu Item contains " + (subMenuItems.size()) + " SubMenus.");
                 System.out.println("------------------------------------------------");
 
                 for (int j = 0; j < subMenuItems.size(); j++) {
-
+                    String subtitle = subMenuItems.get(j).getText();
                     subMenuItems.get(j).click();
                     WebElement h1Element = new WebDriverWait(driver, Duration.ofSeconds(5))
                             .until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h1")));
-                    System.out.println("Clicked on Submenu Item #" + (j + 1) + " under Main Menu Item #" + (i + 1));
-                    System.out.println("Page Title changed to <" + h1Element.getText() + ">.");
+                    System.out.println("Clicked on Submenu Item #" + (j + 1) + " <<" + subtitle + ">> under << " + title + " >>");
+                    System.out.println("Page Title changed to << " + h1Element.getText() + " >>.");
                     //System.out.println("Page Title changed to <" + h1.getText() + ">.");
                     subMenuItems = driver.findElements(By.cssSelector("li[id^=doc-"));
                 }
-                //i++;*/
             }
-
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -102,15 +85,6 @@ public class AssignmentNumber6 {
             System.out.println("driver dismantled");
         }
     }
-    /*@After
-    public void stop () {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
-        }
-        System.out.println("driver dismantled again");
-
-    }*/
 }
 
 
