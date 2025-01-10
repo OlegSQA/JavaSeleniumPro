cd9ac139ab448acb358352a70776d979da32f313
Oleg <webogee@gmail.com>
2025-01-10 13:24:08 -0500

    first test push

--- src/test/java/ru/stqa/AssignmentNumber11.java
+++ src/test/java/ru/stqa/AssignmentNumber11.java
@@ -3,32 +3,64 @@ package ru.stqa;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.ISelect;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
+@RunWith(Parameterized.class)
 public class AssignmentNumber11 {
     private WebDriver driver;
     private WebDriverWait wait;
-    private ISelect select;
+    private final String browser;
 
+    public AssignmentNumber11(String browser) {
+        this.browser = browser;
+    }
+
+    // Define the list of browsers to test
+    @Parameterized.Parameters(name = "Browser: {0}")
+    public static List<String> browsers() {
+        return Arrays.asList("chrome", "firefox", "safari");
+    }
     @Before
     public void start() {
-        driver = new ChromeDriver();
+        System.out.println();
+        System.out.println("Starting test on " + browser);
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "safari":
+                driver = new SafariDriver();
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         wait = new WebDriverWait(driver, Duration.ofSeconds(3));
     }
 
     @Test
     public void MyVeryFirstTest() {
-
         driver.get("http://localhost/litecart/");
         // Generate a unique email address
         String uniqueEmail = "abrakadabra" + System.currentTimeMillis() + "@yahoo.com";
@@ -43,11 +75,11 @@ public class AssignmentNumber11 {
         driver.findElement(By.cssSelector("input[name='address1']")).sendKeys("1 Police Plaza");
         driver.findElement(By.cssSelector("input[name='postcode']")).sendKeys("10038");
         driver.findElement(By.cssSelector("input[name='city']")).sendKeys("New York");
-
         //selecting country
-        WebElement countryDropdown = driver.findElement(By.cssSelector("select[name='country_code']"));
-        Select selectCountry = new Select(countryDropdown);
-        selectCountry.selectByVisibleText("United States");
+        driver.findElement(By.cssSelector("b")).click();
+        driver.findElement(By.cssSelector("input.select2-search__field")).click();
+        driver.findElement(By.cssSelector("input.select2-search__field")).sendKeys("United States");
+        driver.findElement(By.xpath("//li[text()='United States']")).click();
         // selecting state
         WebElement stateDropdown = driver.findElement(By.cssSelector("select[name='zone_code']"));
         Select selectState = new Select(stateDropdown);
